cbc26f25cd5f7eddcb858986238a39a5b7ca684f
Christopher Chateau <christopherchateau@gmail.com>
2018-12-18 13:18:03 -0700

    Add basic syntax to all component and test files

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Start from "../Start";
+import Info from "../Info";
+import Log from "../Log";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentMountain: "",
+      currentDisplay: "start"
+    };
+  }
   render() {
     return (
       <div className="App">
+        <Start />
       </div>
     );
   }
---
+++ src/Start/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Start.css';
+
+class Start extends Component {
+  render() {
+    return (
+      <div className="Start">
+        <select className="drop-down-menu"></select>
+        <button className="sign-log-btn">Sign Log</button>
+      </div>
+    );
+  }
+}
+
+export default Start;

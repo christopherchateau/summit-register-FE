d18d6d7f97d23b3d304f1f485276ef1df2bd296a
Christopher Chateau <christopherchateau@gmail.com>
2018-12-18 13:54:48 -0700

    Add logo to images dir, import to Start component

--- src/Start/index.js
+++ src/Start/index.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
-import './Start.css';
+import React, { Component } from "react";
+import { mountainData } from "../utilities/Data/mountain-data";
+import logo from "../utilities/Images/logo.png";
+import "./Start.css";
 
 class Start extends Component {
   render() {
+    const mountainNames = mountainData.sort().map(name => {
+      return <option value="${name}">{name}</option>;
+    });
+
     return (
       <div className="Start">
-        <select className="drop-down-menu"></select>
+        <h1 className="main-title">Summit Register</h1>
+        <img className="logo" src={logo}/>
+        <div className="drop-down-controls">
+          <select className="drop-down-menu">{mountainNames}</select>
+          <button className="select-btn">Select</button>
+        </div>
         <button className="sign-log-btn">Sign Log</button>
       </div>
     );

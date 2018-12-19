b7ec54ce0f24fcf370e7b1cae07c2682cb163344
Justin Stewart <Justinstewart3313@gmail.com>
2018-12-19 16:15:01 -0700

    Write postToLog func to apiCalls file in assumption of endpoint

--- src/utilities/helper/apiCalls.js
+++ src/utilities/helper/apiCalls.js
@@ -9,3 +9,19 @@ export const getMountainLog = async(id) => {
     const log = await response.json()
     return log
 }
+
+export const postToLog = async(id, name, hometown, comments) => {
+    const response = await fetch(`http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`,
+    {
+        method: "POST",
+        credentials: "same-origin",
+        body: JSON.stringify({
+            name,
+            hometown,
+            comments,
+        }),
+        headers: { "Content-Type": 'application/json'}
+    })
+    const data = await response.json()
+    return data;
+}

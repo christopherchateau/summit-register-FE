a2027ec9e81fe454776eb199de013bf0afc9cc3c
Justin Stewart <Justinstewart3313@gmail.com>
2018-12-19 15:44:38 -0700

    Attempt to fully implement PWA service workers

--- src/index.js
+++ src/index.js
@@ -6,3 +6,18 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
+
+// const checkSW = () => {
+//   if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//       navigator.serviceWorker.register('../public/src/serviceWorkers.js').then(function(registration) {
+//         // Registration was successful
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       }, function(err) {
+//         // registration failed :(
+//         console.log('ServiceWorker registration failed: ', err);
+//       });
+//     });
+//   }
+// }
+// checkSW()

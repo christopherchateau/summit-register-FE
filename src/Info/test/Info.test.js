be6c880f635b7ed08563f5b827713893aa5561bd
Christopher Chateau <christopherchateau@gmail.com>
2018-12-18 17:20:39 -0700

    Add setupTests.js, add basic render/snapshot tests to Info.test and Start.test

---
+++ src/Info/test/Info.test.js
@@ -0,0 +1,34 @@
+configure({ adapter: new Adapter() });
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import Info from "..";
+
+describe("Info", () => {
+  let wrapper;
+  const currentMountainData = {
+    name: "Mt Massive",
+    altitude: 999999,
+    difficulty: "super hard",
+    range: "Front Range"
+  };
+  beforeEach(() => {
+    wrapper = shallow(<Info currentMountainData={currentMountainData} />);
+  });
+
+  it("should exist", () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Info currentMountainData={currentMountainData} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("should match snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});

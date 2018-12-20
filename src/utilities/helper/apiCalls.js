50594d838e8740e2727764e9c051eba61db0505c
Christopher Chateau <christopherchateau@gmail.com>
2018-12-20 12:01:13 -0700

    Run prettier on all files, remove duplicate SignRegister.test.js file

--- src/utilities/helper/apiCalls.js
+++ src/utilities/helper/apiCalls.js
@@ -1,27 +1,33 @@
-export const getMountain = async(id) => {
-    const response = await fetch(`http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`)
-    const mountains = await response.json()
-    return mountains
-}
+export const getMountain = async id => {
+  const response = await fetch(
+    `http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`
+  );
+  const mountains = await response.json();
+  return mountains;
+};
 
-export const getMountainLog = async(id) => {
-    const response = await fetch(`http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`)
-    const log = await response.json()
-    return log
-}
+export const getMountainLog = async id => {
+  const response = await fetch(
+    `http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`
+  );
+  const log = await response.json();
+  return log;
+};
 
-export const postToLog = async(id, name, hometown, comments) => {
-    const response = await fetch(`http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`,
+export const postToLog = async (id, name, hometown, comments) => {
+  const response = await fetch(
+    `http://summit-register-api.herokuapp.com/api/v1/mountains/${id}`,
     {
-        method: "POST",
-        credentials: "same-origin",
-        body: JSON.stringify({
-            name,
-            hometown,
-            comments,
-        }),
-        headers: { "Content-Type": 'application/json'}
-    })
-    const data = await response.json()
-    return data;
-}
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify({
+        name,
+        hometown,
+        comments
+      }),
+      headers: { "Content-Type": "application/json" }
+    }
+  );
+  const data = await response.json();
+  return data;
+};

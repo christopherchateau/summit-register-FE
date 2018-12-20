1eb62cee43e4a27b5126ea11f60cf643cb4fa72f
Christopher Chateau <christopherchateau@gmail.com>
2018-12-19 21:00:56 -0700

    Removed BackButton component from Info, Log and RegisterForm

--- src/Log/index.js
+++ src/Log/index.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import React, { Component } from "react";
 import logo from "../utilities/images/logo.png";
-import BackButton from "../BackButton";
-import SignRegister from '../SignRegister'
-import './Log.css';
+import SignRegister from "../SignRegister";
+import "./Log.css";
 
 class Log extends Component {
   render() {
-    const { name } = this.props.currentMountainLog
-    let singleLog = this.props.currentMountainLog.map( log => {
-      return <li>name: {log.attributes.name}, hometown:{log.attributes.hometown},Date:{log.attributes.date}</li>
-    })
+    const { name } = this.props.currentMountainLog;
+    let singleLog = this.props.currentMountainLog.map(log => {
+      return (
+        <li>
+          name: {log.attributes.name}, hometown:{log.attributes.hometown},Date:
+          {log.attributes.date}
+        </li>
+      );
+    });
 
     if (!singleLog.length) {
-      singleLog = <h2>This Summit has no existing log</h2>
+      singleLog = <h2>This Summit has no existing log</h2>;
     }
 
     return (
       <div className="Log">
         <header className="header">
-          <BackButton handleBackButton={this.props.handleBackButton} />
+          <button className="back-btn" onClick={this.props.handleBackButton}>
+            Back
+          </button>
           <img className="logo-small" alt="logo" src={logo} />
         </header>
         <h1>{name}</h1>
--- src/RegisterForm/index.js
+++ src/RegisterForm/index.js
@@ -1,64 +1,60 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
-import BackButton from '../BackButton'
-import './RegisterForm.css'
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import "./RegisterForm.css";
 
 class RegisterForm extends Component {
   constructor() {
     super();
     this.state = {
-      name: '',
-      hometown: '',
-      comments: ''
-    }
+      name: "",
+      hometown: "",
+      comments: ""
+    };
   }
 
-  componentDidMount() {
-    console.log(this.props.previousPage)
-  }
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-  handleChange = (e) => {
-    const { name, value} = e.target
-    this.setState({[name]: value})
-  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleLogUpdate();
+  };
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.handleLogUpdate()
-  }
-  
   render() {
-    const {name, hometown, comments} = this.state
+    const { name, hometown, comments } = this.state;
     return (
       <div className="RegisterForm">
-      <BackButton handleBackButton={this.props.handleBackButton}/>
-        <form 
-        onSubmit={this.handleSubmit}>
-        <input 
-        className= 'name-input'
-        value={name}
-        name='name'
-        onChange={this.handleChange}
-        ></input>
-        <input 
-        className='hometown-input'
-        value={hometown}
-        name='hometown'
-        onChange={this.handleChange}
-        ></input>
-        <input 
-        className='comments-input'
-        value={comments}
-        name='comments'
-        onChange={this.handleChange}
-        ></input>
-        <button 
-        // onClick={}
-        type='submit' 
-        >
-          Submit Your Entry
+        <button className="back-btn" onClick={this.props.handleBackButton}>
+          Back
         </button>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="name-input"
+            value={name}
+            name="name"
+            onChange={this.handleChange}
+          />
+          <input
+            className="hometown-input"
+            value={hometown}
+            name="hometown"
+            onChange={this.handleChange}
+          />
+          <input
+            className="comments-input"
+            value={comments}
+            name="comments"
+            onChange={this.handleChange}
+          />
+          <button
+            // onClick={}
+            type="submit"
+          >
+            Submit Your Entry
+          </button>
         </form>
       </div>
     );
@@ -69,6 +65,5 @@ RegisterForm.propTypes = {
   currentDisplay: PropTypes.string,
   handleBackButton: PropTypes.func,
   handleLogUpdate: PropTypes.func
-
-}
+};
 export default RegisterForm;

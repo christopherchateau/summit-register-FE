6597b6f22607cef018c1d6bc41a0fb873b176af0
chris <christopherchateau@gmail.com>
2018-12-22 13:55:52 -0700

    Create Header and Footer components

---
+++ src/Footer/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+Footer.propTypes = {
+
+};
+
+export default Footer;

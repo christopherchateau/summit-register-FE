95d625f69f6be6842e66675313fc14fea7d723ed
chris <christopherchateau@gmail.com>
2018-12-26 16:42:02 -0700

    Write 2 tests checking functionality of handleSelectButton in App.test.js

--- src/utilities/helper/__mocks__/apiCalls.js
+++ src/utilities/helper/__mocks__/apiCalls.js
@@ -1,21 +1,36 @@
 export const getMountain = jest.fn().mockImplementation(() => ({
   data: {
+    id: "4",
+    type: "mountain",
     attributes: {
+      altitude: 14345,
+      difficulty: "Black",
+      name: "Blanca Peak",
+      range: "Sangre de Cristo",
+      summit: "37.577473,-105.485443",
       registries: {
         data: [
           {
-            name: "Chris",
-            hometown: "Detroit",
-            comments: "Hiking rules",
-            date: "2018-12-26 20:03:00 UTC",
-            sign_time: "26/12/2018 @ 13:2:59"
+            id: "5",
+            type: "registry",
+            attributes: {
+              name: "Chris",
+              hometown: "Detroit",
+              comments: "Hiking rules",
+              date: "2018-12-26 20:03:00 UTC",
+              sign_time: "26/12/2018 @ 13:2:59"
+            }
           },
           {
-            name: "Justin",
-            hometown: "Denver",
-            comments: "14ers r sooper lit",
-            date: "2018-12-25 20:05:00 UTC",
-            sign_time: "25/12/2018 @ 13:4:59"
+            id: "6",
+            type: "registry",
+            attributes: {
+              name: "Justin",
+              hometown: "Denver",
+              comments: "14ers r sooper lit",
+              date: "2018-12-25 20:05:00 UTC",
+              sign_time: "25/12/2018 @ 13:4:59"
+            }
           }
         ]
       }

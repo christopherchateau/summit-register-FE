0d64c545bcbe36e82f946be8c93d5c7b967e02c4
chris <christopherchateau@gmail.com>
2018-12-26 17:27:46 -0700

    Write 3 tests checking functionality of handleLogUpdate in App.test.js

--- src/utilities/helper/__mocks__/apiCalls.js
+++ src/utilities/helper/__mocks__/apiCalls.js
@@ -37,3 +37,28 @@ export const getMountain = jest.fn().mockImplementation(() => ({
     }
   }
 }));
+
+export const postToLog = jest.fn().mockImplementation(() => [
+  {
+    id: "5",
+    type: "registry",
+    attributes: {
+      name: "Andrew M",
+      hometown: "Longmont",
+      comments: "Hiking rules",
+      date: "2018-12-26 20:03:00 UTC",
+      sign_time: "26/12/2018 @ 13:2:59"
+    }
+  },
+  {
+    id: "6",
+    type: "registry",
+    attributes: {
+      name: "Andrew T",
+      hometown: "Highlands Ranch",
+      comments: "14ers r sooper lit",
+      date: "2018-12-25 20:05:00 UTC",
+      sign_time: "25/12/2018 @ 13:4:59"
+    }
+  }
+]);

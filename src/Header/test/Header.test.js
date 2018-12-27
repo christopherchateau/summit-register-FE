40f53584a08a38ff56b9422faecbfaa9f0424935
chris <christopherchateau@gmail.com>
2018-12-26 20:36:14 -0700

    Write 2 tests checking display text for Header.test.js

--- src/Header/test/Header.test.js
+++ src/Header/test/Header.test.js
@@ -1,9 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from '..';
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "..";
+import { shallow } from "enzyme";
 
-it.skip('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Header/>, div);
-  ReactDOM.unmountComponentAtNode(div); 
+describe("Header", () => {
+  let wrapper;
+  const currentMountainData = {
+    attributes: {
+      altitude: 14265,
+      difficulty: "Blue",
+      name: "Castle Peak",
+      range: "Elk",
+      registries: { data: [] },
+      summit: "39.009647,-106.86144"
+    },
+    id: "12",
+    type: "mountain"
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <Header
+        currentDisplay={"start"}
+        handleBackButton={jest.fn()}
+        currentMountainData={currentMountainData}
+      />
+    );
+  });
+
+  it("should match snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <Header
+        currentDisplay={"start"}
+        handleBackButton={jest.fn()}
+        currentMountainData={currentMountainData}
+      />,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  describe("display text", () => {
+    it("should display 'summit register' on start screen", () => {
+      const displayText = wrapper.find(".main-title");
+      expect(displayText.text()).toBe("Summit Register");
+    });
+
+    it("should display mountain name if not on start screen", () => {
+      wrapper = shallow(
+        <Header
+          currentDisplay={"info"}
+          handleBackButton={jest.fn()}
+          currentMountainData={currentMountainData}
+        />
+      );
+      const displayText = wrapper.find(".main-title");
+      expect(displayText.text()).toBe("Castle Peak");
+    });
+  });
 });

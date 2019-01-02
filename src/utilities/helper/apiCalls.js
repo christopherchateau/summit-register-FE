06c530772f4595aac7cfb2253ef1f2ed68de5056
chris <christopherchateau@gmail.com>
2019-01-02 16:17:15 -0700

    Add postImage func to apiCalls to post image to cloudinary. Add event listener to image input in RegisterForm to update state with image details

--- src/utilities/helper/apiCalls.js
+++ src/utilities/helper/apiCalls.js
@@ -9,12 +9,14 @@ export const getMountain = async id => {
 };
 
 export const getCurrentUser = () => {
-  return firebase.auth().currentUser
-}
+  return firebase.auth().currentUser;
+};
 
-export const getWeather = async (location) => {
+export const getWeather = async location => {
   const response = await fetch(
-    `https://summit-register-weather-api.herokuapp.com/?lat=${location[0]}&lon=${location[1]}`
+    `https://summit-register-weather-api.herokuapp.com/?lat=${
+      location[0]
+    }&lon=${location[1]}`
   );
   const weather = await response.json();
   return weather;
@@ -38,4 +40,23 @@ export const postToLog = async (id, logEntry, timeStamp) => {
   );
   const updatedLog = await response.json();
   return updatedLog.data;
+};
+
+export const postImage = async image => {
+  const formData = new FormData();
+  const cloudinaryPreset = "qke5dhjq";
+
+  formData.append("file", image);
+  formData.append("upload_preset", cloudinaryPreset);
+
+  const response = await fetch(
+    "https://api.cloudinary.com/v1_1/summit-register/upload",
+    {
+      method: "POST",
+      body: formData
+    }
+  );
+  const data = await response.json();
+  console.log(data)
+  return data.secure_url;
 };

d682cdfabd7e8bfe6070c6c51692ca6e402cdfff
chris <christopherchateau@gmail.com>
2019-01-07 12:48:24 -0700

    Add 'a_ignore/' to url in postImage to fix bug with pics rotating on cloudinary

--- src/HourlyForecast/test/HourlyForecast.test.js
+++ src/HourlyForecast/test/HourlyForecast.test.js
@@ -14,6 +14,7 @@ describe("HourlyForecast", () => {
     humidity: 0.95,
     windSpeed: 12.39
   };
+  
   beforeEach(() => {
     wrapper = shallow(<HourlyForecast {...currentMountainWeather} />);
   });

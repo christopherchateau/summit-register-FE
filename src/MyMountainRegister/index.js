b4072490f20324620da0c78a818fc2299a235ed3
chris <christopherchateau@gmail.com>
2019-01-09 10:18:40 -0700

    Tweak styling of MyMountain: only render <hr /> if log entry isn't not first for that mountain, move picture to top, add margin-bottom to mountain name

--- src/MyMountainRegister/index.js
+++ src/MyMountainRegister/index.js
@@ -4,15 +4,22 @@ import "./MyMountainRegister.css";
 
 class MyMountainRegister extends Component {
   render() {
-    const { name, hometown, comments, sign_time, image_url } = this.props.log;
+    const {
+      name,
+      hometown,
+      comments,
+      sign_time,
+      image_url,
+      index
+    } = this.props.log;
     return (
       <div className="log-entry">
-        <hr />
+        {this.props.index !== 0 && <hr />}
+        {image_url && <img className="user-image" alt="user" src={image_url} />}
         <h4>Name: {name}</h4>
         <h4>Hometown: {hometown}</h4>
         <h4>Comments: {comments} </h4>
         <h4>Date: {sign_time}</h4>
-        {image_url && <img className="user-image" alt="user" src={image_url} />}
       </div>
     );
   }
